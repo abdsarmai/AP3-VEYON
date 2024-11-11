9ec2d7c23a817d9f5c083b06859ec2b6dafa4fc4
abdsarmai <88580232+abdsarmai@users.noreply.github.com>
2024-11-11 11:28:53 +0100

    Update deploy_veyon.bat

--- deploy_veyon.bat
+++ deploy_veyon.bat
@@ -4,7 +4,6 @@ setlocal enabledelayedexpansion
 echo Installation de Veyon en cours...
 echo.
 
-:: Vérifier si le script est exécuté en tant qu'administrateur
 NET SESSION >nul 2>&1
 if %errorLevel% neq 0 (
     echo Ce script necessite des droits administrateur.
@@ -13,11 +12,9 @@ if %errorLevel% neq 0 (
     exit /b 1
 )
 
-:: Afficher les informations de débogage
 echo Repertoire du script : %~dp0
 echo Fichier recherche : %~dp0veyon-4.9.0.0-win64-setup.exe
 
-:: Vérifier si le fichier d'installation existe
 if not exist "%~dp0veyon-4.9.0.0-win64-setup.exe" (
     echo Le fichier veyon-4.9.0.0-win64-setup.exe n'a pas ete trouve.
     echo Chemin complet recherche : %~dp0veyon-4.9.0.0-win64-setup.exe
@@ -28,10 +25,8 @@ if not exist "%~dp0veyon-4.9.0.0-win64-setup.exe" (
 echo Installation silencieuse de Veyon...
 "%~dp0veyon-4.9.0.0-win64-setup.exe" /S
 
-:: Attendre que l'installation se termine
 timeout /t 10 /nobreak
 
-:: Vérifier si l'installation a réussi
 if exist "C:\Program Files\Veyon" (
     echo Installation de Veyon terminee avec succes!
 ) else (
